e4042f685776706e52d447e52d12b7a64b5aeb41
jakegottfried <jacobgottfried22@gmail.com>
2014-12-23 17:42:20 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 
+function product1(){
+	$('#desc').html("Bianchi's Cortina Dama is perfect for getting around town and campus, running errands, commuting in style, and just having a great time outdoors. It delivers a sweet ride thanks to the light aluminum frame, easy-rolling 700c wheels, and plush seat. Plus, with its hill-topping 24-speed drivetrain, powerful brakes, and comfortable riding position, the Cortina is perfect for every adventure");
+	$('#pic').html("<img src='img/bike1.jpg'  />");
+}
+
+function product2(){
+	$('#desc').html("There have always been city road bikes lovingly locked to lampposts and street signs in major urban centers or around college campuses. That's because of their high durability paired with high style. Designed city-tough and made exclusively for Performance Bicycle, the Charge Plug road bike features a pavement specific build that will keep you comfortable and feeling great even on the toughest of pothole laden city streets.The Charge Plug is set up by city riders for city riders. It's got a stylish steel frame that keeps the Plug sleek-looking while having the added benefit of steel's natural vibration dampening properties. The compact Shimano Claris 16-speed drivetrain means you can do more than the average urban bike can; you can climb. While many urban bikes are designed for grades that have little variation, the Plug can handle big ascents and descents, e.g. San Francisco or Seattle. The tough alloy wheelset is equipped with a pair of Kenda Small Block 8 tires which adds even more off-road versatility. The Small Block 8's grab and hold dirt or gravel surfaces without sacrificing the required low rolling resistance of street riding.");
+	$('#pic').html("<img src='img/bike1.jpg'  />");
+}
+
+function product3(){
+	$('#desc').html("Imagine rolling along on your favorite road loop when you see a gravel road you've never noticed before. Maybe you've ridden by it a hundred times before. Maybe you've strayed farther from home than ever before. Either way, you know adventure waits for you down that road. You can feel it. Luckily, you're on your trusty Charge Plug 4. As you turn onto the gravel road, you immediately notice the smooth ride of the Kenda Kwick Roller Tendril tires. Sure, the guys in the Tour De France may be running 23mm wide tires, but that's not you. You're running 28mm tires and the extra traction and comfort pay off in spades. You feel fresh and alive as the Tange Infinity steel frame and fork soak up the vibrations from the rougher road. The Promax disc brakes have you controlling your speed with confidence and the wide range of the Shimano 105 drivetrain gives you plenty of gearing options as the road winds up and over hills and mountains.");
+	$('#pic').html("<img src='img/bike1.jpg'  />");
+}
+
+function product4(){
+	$('#desc').html("Whether en route for that early A.M. caffeine kick at your local coffee stop or you're drifting to the office, this low maintenance, single-speed city bike is just what you need to get you there and back for the rest of the day's adventures. Hi-Ten steel frame gives a smooth, responsive ride through city streets. High-profile, double wall rims with Vera CityWide tires grip the asphalt. Riser bars and comfy saddle put you in an upright riding position while Shimano coaster brakes stop you every time. Integrated bottle opener under the saddle is perfect for cracking open a cold one. Let's hear it for the simple life.");
+	$('#pic').html("<img src='img/bike1.jpg'  />");
+}
+
+function product5(){
+	$('#desc').html("Why just ride a bike when you can look great doing it too?. SE Racing has revamped the Lager Single-Speed for 2015, with a tasty new colors and the single-speed simplicity we all know and love. The cream of the crop of their city bikes, the SE Lager City epitomizes the spirit of single speeds with a slightly laid back steel frame with a chromoly seat tube, and a hi-tensile steel tapered straight blade fork");
+	$('#pic').html("<img src='img/bike1.jpg'  />");
+}
+
+function product6(){
+	$('#desc').html("The Breezer Downtown 8 City Bike is comfortable, efficient, and an all-around pleasure to ride. It's perfect for around town or neighborhood riding for students and commuters. A chromoly frame and a host of aluminum components keep the bike lightweight and maneuverable. A Shimano drivetrain means shifting technology from some of the best engineers in the industry with 8-speeds to give you options for climbing, accelerating, and sustaining higher speeds.");
+	$('#pic').html("<img src='img/bike1.jpg'  />");
+}
+
